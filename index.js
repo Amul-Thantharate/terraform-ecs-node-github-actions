72762c50d9e96b1e8135ba1d80041207dca9fa64
Amul-Thantharate <amulthantharate@gmail.com>
2024-05-19 18:46:31 +0530

    chore: Update AWS.yml and index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send("Hello World form Github Actions!");
+    res.send("Hello World form Github Actions! 2nd time!");
 });
 
 app.listen(80);
